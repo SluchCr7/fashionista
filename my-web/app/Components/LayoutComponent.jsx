ecd0cb7dd9ff341b5b097df917a9c2d90267714c
Sluch <ahmedking10710@gmail.com>
2025-11-04 12:15:49 +0200

    the Final Update

--- my-web/app/Components/LayoutComponent.jsx
+++ my-web/app/Components/LayoutComponent.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Loader from './Loader';
