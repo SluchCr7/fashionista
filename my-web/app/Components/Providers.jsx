965353b73691bf2b90e13c2ad8842968e30d561b
Sluch <ahmedking10710@gmail.com>
2026-02-07 21:19:19 +0200

    new update at Levels page

---
+++ my-web/app/Components/Providers.jsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React from 'react';
+import AdContextProvider from "../Context/AdsContext";
+import CartContextProvider from "../Context/Cart";
+import ProductContextProvider from "../Context/ProductContext";
+import ReviewContextProvider from "../Context/ReviewContext";
+import UserContextProvider from "../Context/UserContext";
+import ThemeProvider from "../Context/ThemeContext";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import LayoutComponent from "./LayoutComponent";
+
+export default function Providers({ children }) {
+    return (
+        <ThemeProvider>
+            <UserContextProvider>
+                <CartContextProvider>
+                    <ProductContextProvider>
+                        <AdContextProvider>
+                            <ReviewContextProvider>
+                                <LayoutComponent>
+                                    {children}
+                                </LayoutComponent>
+                            </ReviewContextProvider>
+                        </AdContextProvider>
+                    </ProductContextProvider>
+                </CartContextProvider>
+            </UserContextProvider>
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                style={{ zIndex: 99999 }}
+            />
+        </ThemeProvider>
+    );
+}
--- my-web/app/layout.js
+++ my-web/app/layout.js
@@ -1,17 +1,6 @@
-'use client';
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import LayoutComponent from "./Components/LayoutComponent";
-import AdContextProvider from "./Context/AdsContext";
-import CartContextProvider from "./Context/Cart";
-import ProductContextProvider from "./Context/ProductContext";
-import ReviewContextProvider, { ReviewContext } from "./Context/ReviewContext";
-import UserContextProvider from "./Context/UserContext";
-import ThemeProvider from "./Context/ThemeContext";
 import "./globals.css";
-import "react-toastify/dist/ReactToastify.css";
+import Providers from "./Components/Providers";
 import { Inter, Playfair_Display } from "next/font/google";
-import { ToastContainer } from "react-toastify";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,6 +14,7 @@ const playfair = Playfair_Display({
   display: "swap",
 });
 
+// Since this is now a Server Component, exporting metadata is allowed
 export const metadata = {
   title: `Fashionista | Premium Fashion`,
   description: "Discover the latest trends in high-end fashion with Fashionista.",
@@ -36,34 +26,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.variable} ${playfair.variable} font-sans bg-background text-foreground antialiased`}
       >
-        <ThemeProvider>
-          <UserContextProvider>
-            <CartContextProvider>
-              <ProductContextProvider>
-                <AdContextProvider>
-                  <ReviewContextProvider>
-                    <LayoutComponent>
-                      {children}
-                    </LayoutComponent>
-                  </ReviewContextProvider>
-                </AdContextProvider>
-              </ProductContextProvider>
-            </CartContextProvider>
-          </UserContextProvider>
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            style={{ zIndex: 99999 }}
-          />
-        </ThemeProvider>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
